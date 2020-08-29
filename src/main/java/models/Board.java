5efb1275c36c3b896fb220ec50500826fddb6089
Baalber <bartalberts@hotmail.com>
2020-08-29 18:46:50 +0200

    eerste aanzet board

---
+++ src/main/java/models/Board.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Board {
+    String println;
+
+    public Board() {
+    println = "piemelbier";
+    }
+
+    public String getPrintln() {
+        return println;
+    }
+
+    public void setPrintln(String piemel) {
+        println = piemel;
+    }
+}
