d5486d262ad56290b13e478d9bed5578e4747182
MartijnSlot <martijn.slot@nedap.com>
2020-09-21 11:30:34 +0200

    generieke test weggegooid

---
+++ src/test/java/checkmaat/AppTest.java
@@ -0,0 +1,10 @@
+package checkmaat;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testAppHasAGreeting() {
+        assertEquals(5, 5);
+    }
+}
